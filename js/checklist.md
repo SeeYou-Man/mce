8886ba473a28196a9847ba236f07a60b9221d52d
SeeYou-Man <49405094+SeeYou-Man@users.noreply.github.com>
2023-10-26 22:05:18 +0200

    Added all 40 enchantments, and give right Weight

--- js/checklist.md
+++ js/checklist.md
@@ -15,49 +15,49 @@ Treasure Enchantment = States incase the enchantment is a treasure enchantment (
 
 From Fishing = States incase the enchantment is possible to get from fishing (Yes/No)
 
-Note: The table is sorted Aplhabetically, enchantments.js is sorted by ID low --> high
+Note: The table is sorted Aplhabetically, enchantments.js is sorted by ID low --> high, The ID is given from grouping them first
 
 ### List
 
 | Enchantment Name      | Enchantment Implemented | Enchantment ID | Enchantment Weight | Treasure Enchantment |
 | --------------------- | ----------------------- | -------------- | ------------------ | -------------------- |
-| Aqua Affinity         | No                      | 9              | 2                  | No                   |
-| Bane of Arthropods    | No                      |                | 5                  | No                   |
-| Blast Protection      | No                      |                | 2                  | No                   |
-| Channeling            | No                      |                | 1                  | No                   |
-| Cleaving              | No                      |                | 0                  | Unknown              |
-| Curse of Binding      | No                      |                | 1                  | Yes                  |
-| Curse of Vanishing    | No                      |                | 1                  | Yes                  |
-| Depth Strider         | No                      |                | 2                  | No                   |
-| Efficiency            | No                      |                | 10                 | No                   |
-| Feather Falling       | No                      |                | 5                  | No                   |
-| Fire Aspect           | No                      |                | 2                  | No                   |
-| Fire Protection       | No                      |                | 5                  | No                   |
-| Flame                 | No                      |                | 2                  | No                   |
-| Fortune               | No                      |                | 2                  | No                   |
-| Frost Walker          | No                      |                | 2                  | Yes                  |
-| Impaling              | No                      |                | 2                  | No                   |
-| Infinity              | No                      |                | 1                  | No                   |
-| Knockback             | No                      |                | 5                  | No                   |
-| Looting               | No                      |                | 2                  | No                   |
-| Loyalty               | No                      |                | 5                  | No                   |
-| Luck of the Sea       | No                      |                | 2                  | No                   |
-| Lure                  | No                      |                | 2                  | No                   |
-| Mending               | No                      |                | 2                  | Yes                  |
-| Multishot             | No                      |                | 2                  | No                   |
-| Piercing              | No                      |                | 10                 | No                   |
-| Power                 | No                      |                | 10                 | No                   |
-| Projectile Protection | No                      |                | 5                  | No                   |
-| Protection            | No                      |                | 10                 | No                   |
-| Punch                 | No                      |                | 2                  | No                   |
-| Quick Charge          | No                      |                | 5                  | No                   |
-| Respiration           | No                      |                | 2                  | No                   |
-| Riptide               | No                      |                | 2                  | No                   |
-| Sharpness             | No                      |                | 10                 | No                   |
-| Silk Touch            | No                      |                | 1                  | No                   |
-| Smite                 | No                      |                | 5                  | No                   |
-| Soul Speed            | No                      |                | 1                  | Yes                  |
-| Sweeping Edge         | No                      |                | 2                  | No                   |
-| Swift Sneak           | No                      |                | 1                  | Yes                  |
-| Thorns                | No                      |                | 1                  | No                   |
-| Unbreaking            | No                      |                | 5                  | No                   |
+| Aqua Affinity         | No                      | 6              | 2                  | No                   |
+| Bane of Arthropods    | No                      | 15             | 5                  | No                   |
+| Blast Protection      | No                      | 2              | 2                  | No                   |
+| Channeling            | No                      | 33             | 1                  | No                   |
+| Cleaving              | No                      | 20             | 0                  | Unknown              |
+| Curse of Binding      | No                      | 10             | 1                  | Yes                  |
+| Curse of Vanishing    | No                      | 32             | 1                  | Yes                  |
+| Depth Strider         | No                      | 8              | 2                  | No                   |
+| Efficiency            | No                      | 25             | 10                 | No                   |
+| Feather Falling       | No                      | 4              | 5                  | No                   |
+| Fire Aspect           | No                      | 17             | 2                  | No                   |
+| Fire Protection       | No                      | 1              | 5                  | No                   |
+| Flame                 | No                      | 23             | 2                  | No                   |
+| Fortune               | No                      | 26             | 2                  | No                   |
+| Frost Walker          | No                      | 9              | 2                  | Yes                  |
+| Impaling              | No                      | 34             | 2                  | No                   |
+| Infinity              | No                      | 24             | 1                  | No                   |
+| Knockback             | No                      | 16             | 5                  | No                   |
+| Looting               | No                      | 18             | 2                  | No                   |
+| Loyalty               | No                      | 35             | 5                  | No                   |
+| Luck of the Sea       | No                      | 28             | 2                  | No                   |
+| Lure                  | No                      | 29             | 2                  | No                   |
+| Mending               | No                      | 31             | 2                  | Yes                  |
+| Multishot             | No                      | 37             | 2                  | No                   |
+| Piercing              | No                      | 38             | 10                 | No                   |
+| Power                 | No                      | 21             | 10                 | No                   |
+| Projectile Protection | No                      | 3              | 5                  | No                   |
+| Protection            | No                      | 0              | 10                 | No                   |
+| Punch                 | No                      | 22             | 2                  | No                   |
+| Quick Charge          | No                      | 39             | 5                  | No                   |
+| Respiration           | No                      | 5              | 2                  | No                   |
+| Riptide               | No                      | 36             | 2                  | No                   |
+| Sharpness             | No                      | 13             | 10                 | No                   |
+| Silk Touch            | No                      | 27             | 1                  | No                   |
+| Smite                 | No                      | 14             | 5                  | No                   |
+| Soul Speed            | No                      | 11             | 1                  | Yes                  |
+| Sweeping Edge         | No                      | 19             | 2                  | No                   |
+| Swift Sneak           | No                      | 12             | 1                  | Yes                  |
+| Thorns                | No                      | 7              | 1                  | No                   |
+| Unbreaking            | No                      | 30             | 5                  | No                   |
--- js/enchantments.js
+++ js/enchantments.js
@@ -8,66 +8,6 @@
  */
 var _enchantments = [
 	{
-		name: "unbreaking",
-		weight: 5,
-		minlevel: 1,
-		maxlevel: 3,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
-		minEnchant: function( i ){  return 5+(i-1)*8;},
-		canEnchant: function(mId,iId) { return iId >= 0 && iId <=14},
-		applies: function( other ) { return other !== "unbreaking" },
-		id: 0,
-		color: "turquoise"
-	},
-	{
-		name: "mending",
-		weight: 1,
-		minlevel: 1,
-		maxlevel: 1,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
-		minEnchant: function( i ){  return 5+(i-1)*8;},
-		canEnchant: function(mId,iId) { return iId === 1; },
-		applies: function( other ) { return other !== "mending" || other !== "infinity" },
-		id: 1,
-		color: "turquoise"
-	},
-	{
-		name: "efficiency",
-		weight: 10,
-		minlevel: 1,
-		maxlevel: 5,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
-		minEnchant: function( i ){  return 1+(i-1)*10;},
-		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 4; },
-		applies: function( other ) { return !( other === "efficiency" ) },
-		id: 2,
-		color: "orchid"
-	},
-	{
-		name: "fortune",
-		weight: 2,
-		minlevel: 1,
-		maxlevel: 3,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
-		minEnchant: function( i ){  return 15+(i-1)*9;},
-		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 4; },
-		applies: function( other ) { return !( other === "fortune" || other === "silktouch" ) },
-		id: 3,
-		color: "white"
-	},
-	{
-		name: "silktouch",
-		weight: 1,
-		minlevel: 1,
-		maxlevel: 1,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
-		minEnchant: function(){  return 15;},
-		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 5; },
-		applies: function( other ) { return !( other === "fortune" || other === "silktouch" || other === "looting" || other === "luckofthesea" ) },
-		id: 4,
-		color: "darkviolet"
-	},
-	{
 		name: "protection",
 		weight: 10,
 		minlevel: 1,
@@ -76,7 +16,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 1+(i-1)*11; },
 		canEnchant: function(mId,iId) { return iId === 0 || iId === 1 || iId >= 0 && iId <= 5; },
 		applies: function( other ) { return !( other === "protection" || other === "fireprotection" || other === "blastprotection" ||other === "projectileprotection" ) },
-		id: 5,
+		id: 0,
 		color: "blue"
 	},
 	{
@@ -88,7 +28,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 10+(i-1)*8;},
 		canEnchant: function(mId,iId) { return iId === 5 || iId === 6 || iId === 7 || iId === 8; },
 		applies: function( other ) { return !( other === "protection" || other === "fireprotection" || other === "blastprotection" ||other === "projectileprotection" ) },
-		id: 6,
+		id: 1,
 		color: "red"
 	},
 	{
@@ -100,7 +40,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 5+(i-1)*8;},
 		canEnchant: function(mId,iId) { return iId === 5 || iId === 6 || iId === 7 || iId === 8; },
 		applies: function( other ) { return !( other === "protection" || other === "fireprotection" || other === "blastprotection" ||other === "projectileprotection" ) },
-		id: 7,
+		id: 2,
 		color: "black"
 	},
 	{
@@ -112,20 +52,20 @@ var _enchantments = [
 		minEnchant: function( i ){  return 3+(i-1)*6;},
 		canEnchant: function(mId,iId) { return iId === 5 || iId === 6 || iId === 7 || iId === 8; },
 		applies: function( other ) { return !( other === "protection" || other === "fireprotection" || other === "blastprotection" ||other === "projectileprotection" ) },
-		id: 8,
+		id: 3,
 		color: "green"
 	},
 	{
-		name: "aquaaffinity",
-		weight: 2,
+		name: "featherfalling",
+		weight: 5,
 		minlevel: 1,
-		maxlevel: 1,
-		maxEnchant: function(){ return 41 },
-		minEnchant: function(){  return 1},
-		canEnchant: function(mId,iId) { return iId === 4; },
-		applies: function( other ) { return other !== "aquaaffinity"  },
-		id: 9,
-		color: "teal"
+		maxlevel: 4,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 10;},
+		minEnchant: function( i ){  return 5+(i-1)*6;},
+		canEnchant: function(mId,iId) { return iId === 8 },
+		applies: function( other ) { return other !== "featherfalling" },
+		id: 4,
+		color: "grey"
 	},
 	{
 		name: "respiration",
@@ -136,84 +76,98 @@ var _enchantments = [
 		minEnchant: function( i ){  return i*10;},
 		canEnchant: function(mId,iId) { return iId === 4 },
 		applies: function( other ) { return  other !== "respiration" },
-		id: 10,
+		id: 5,
 		color: "lime"
 	},
 	{
-		name: "thorns",
+		name: "aquaaffinity",
 		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function(){ return 41 },
+		minEnchant: function(){  return 1},
+		canEnchant: function(mId,iId) { return iId === 4; },
+		applies: function( other ) { return other !== "aquaaffinity"  },
+		id: 6,
+		color: "teal"
+	},
+	{
+		name: "thorns",
+		weight: 1,
 		minlevel: 1,
 		maxlevel: 3,
 		maxEnchant: function( i ){ return this.minEnchant(i) + 30;},
 		minEnchant: function( i ){  return i*10;},
 		canEnchant: function(mId,iId) { return iId === 4 },
 		applies: function( other ) { return  other !== "thorns" },
-		id: 10,
+		id: 7,
 		color: "lime"
-	},
-	/** Swift Sneak
-	 * To be implemented since it cannot be obtained through normal enchanting thinking of making a possibility to enable surtain enchantments
-	{
-		name: "swiftsneak",
-		weight: 2,
-		minlevel: 1,
-		maxlevel: 3,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 30;},
-		minEnchant: function( i ){  return i*10;},
-		canEnchant: function(mId,iId) { return iId === 4 },
-		applies: function( other ) { return  other !== "swiftssneak" },
-		id: 11,
-		color: "lime"
-	},
-	 */
-	{
-		name: "featherfalling",
-		weight: 5,
-		minlevel: 1,
-		maxlevel: 4,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 10;},
-		minEnchant: function( i ){  return 5+(i-1)*6;},
-		canEnchant: function(mId,iId) { return iId === 8 },
-		applies: function( other ) { return other !== "featherfalling" },
-		id: 12,
-		color: "grey"
 	},
 	{
 		name: "depthstrider",
-		weight: 5,
+		weight: 2,
 		minlevel: 1,
 		maxlevel: 4,
 		maxEnchant: function( i ){ return this.minEnchant(i) + 10;},
 		minEnchant: function( i ){  return 5+(i-1)*6;},
 		canEnchant: function(mId,iId) { return iId === 10 || (iId >=0 && iId <=1) },
 		applies: function( other ) { return other !==("depthstrider" || "frostwalker") }, //Unsure if this works trying a new way of unincluding enchantments, this would require the least amounts of characters
-		id: 13,
+		id: 8,
 		color: "grey"
 	},
 	{
 		name: "frostwalker",
-		weight: 5,
+		weight: 2,
 		minlevel: 1,
 		maxlevel: 4,
 		maxEnchant: function( i ){ return this.minEnchant(i) + 10;},
 		minEnchant: function( i ){  return 5+(i-1)*6;},
 		canEnchant: function(mId,iId) { return iId === 10 || iId === 1 },
 		applies: function( other ) { return other !== "frostwalker" || other !== "depthstrider"},
-		id: 14,
+		id: 9,
 		color: "grey"
 	},
 	{
+		name: "curseofbinding",
+		weight: 1,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "curseofbinding" },
+		id: 10,
+		color: "pink"
+	},
+	/** 
+	 * Swift Sneak and Soulspeed
+	 * To be implemented since it cannot be obtained through normal enchanting thinking of making a possibility to enable surtain enchantments
+	{
 		name: "soulspeed",
-		weight: 5,
+		weight: 1,
 		minlevel: 1,
 		maxlevel: 4,
 		maxEnchant: function( i ){ return this.minEnchant(i) + 10;},
 		minEnchant: function( i ){  return 5+(i-1)*6;},
 		canEnchant: function(mId,iId) { return iId === 10 || iId === 1 },
 		applies: function( other ) { return other !== "soulspeed"},
-		id: 15,
+		id: 11,
 		color: "grey"
 	},
+	
+	{
+		name: "swiftsneak",
+		weight: 1,
+		minlevel: 1,
+		maxlevel: 3,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 30;},
+		minEnchant: function( i ){  return i*10;},
+		canEnchant: function(mId,iId) { return iId === 4 },
+		applies: function( other ) { return  other !== "swiftssneak" },
+		id: 12,
+		color: "lime"
+	},
+	 */
 	{
 		name: "sharpness",
 		weight: 10,
@@ -223,7 +177,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 1+(i-1)*11;},
 		canEnchant: function(mId,iId) { return iId === 1 ; },
 		applies: function( other ) { return !( other === "baneofarthropods" || other === "smite" || other === "sharpness" ) },
-		id: 16,
+		id: 13,
 		color: "yellow" },
 	{
 		name: "smite",
@@ -234,7 +188,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 5+(i-1)*8;},
 		canEnchant: function(mId,iId) { return iId === 1; },
 		applies: function( other ) { return !(other === "baneofarthropods" || other === "smite" || other === "sharpness") },
-		id: 17,
+		id: 14,
 		color: "sienna"
 	},
 	{
@@ -246,7 +200,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 5+(i-1)*8;},
 		canEnchant: function(mId,iId) { return iId === 1; },
 		applies: function( other ) { return !( other === "baneofarthropods" || other === "smite" || other === "sharpness" ) },
-		id: 18,
+		id: 15,
 		color: "olive"
 	},
 	{
@@ -258,7 +212,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 5+(i-1)*20;},
 		canEnchant: function(mId,iId) { return iId === 1; },
 		applies: function( other ) { return other !== "knockback" },
-		id: 19,
+		id: 16,
 		color: "lightblue"
 	},
 	{
@@ -270,7 +224,7 @@ var _enchantments = [
 		minEnchant: function( i ){  return 10+(i-1)*20;},
 		canEnchant: function(mId,iId) { return iId === 1; },
 		applies: function( other ) { return other !== "fireaspect" },
-		id: 20,
+		id: 17,
 		color: "orange"
 	},
 	{
@@ -282,9 +236,36 @@ var _enchantments = [
 		minEnchant: function( i ){  return 20+(i-1)*12;},
 		canEnchant: function(mId,iId) { return iId === 1; },
 		applies: function( other ) { return other !== "looting" },
-		id: 21,
+		id: 18,
 		color: "navy"
 	},
+	{
+		name: "sweepingedge",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "sweepingedge" },
+		id: 19,
+		color: "pink"
+	},
+	/** Since Cleaving is not out yet and we don't know more about it I cannot implement it futher as of 1.20.2
+	{
+		name: "cleaving",
+		weight: 0,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "cleaving" },
+		id: 20,
+		color: "pink"
+	},
+	*/
+	
 	{
 		name: "power",
 		weight: 10,
@@ -294,8 +275,20 @@ var _enchantments = [
 		minEnchant: function( i ){  return 1+(i-1)*10;},
 		canEnchant: function(mId,iId) { return iId === 8; },
 		applies: function( other ) { return other !== "power" },
-		id: 22,
+		id: 21,
 		color: "yellow"
+	},
+	{
+		name: "punch",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 2,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 25;},
+		minEnchant: function( i ){  return 12+(i-1)*20;},
+		canEnchant: function(mId,iId) { return iId === 8; },
+		applies: function( other ) { return other !== "punch" },
+		id: 22,
+		color: "lightblue"
 	},
 	{
 		name: "flame",
@@ -310,18 +303,6 @@ var _enchantments = [
 		color: "orange"
 	},
 	{
-		name: "punch",
-		weight: 2,
-		minlevel: 1,
-		maxlevel: 2,
-		maxEnchant: function( i ){ return this.minEnchant(i) + 25;},
-		minEnchant: function( i ){  return 12+(i-1)*20;},
-		canEnchant: function(mId,iId) { return iId === 8; },
-		applies: function( other ) { return other !== "punch" },
-		id: 24,
-		color: "lightblue"
-	},
-	{
 		name: "infinity",
 		weight: 1,
 		minlevel: 1,
@@ -330,7 +311,188 @@ var _enchantments = [
 		minEnchant: function( i ){  return 20;},
 		canEnchant: function(mId,iId) { return iId === 8; },
 		applies: function( other ) { return other !== "infinity" },
-		id: 25,
+		id: 24,
 		color: "pink"
-	}
+	},
+	{
+		name: "efficiency",
+		weight: 10,
+		minlevel: 1,
+		maxlevel: 5,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
+		minEnchant: function( i ){  return 1+(i-1)*10;},
+		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 4; },
+		applies: function( other ) { return !( other === "efficiency" ) },
+		id: 25,
+		color: "orchid"
+	},
+	{
+		name: "fortune",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 3,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
+		minEnchant: function( i ){  return 15+(i-1)*9;},
+		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 4; },
+		applies: function( other ) { return !( other === "fortune" || other === "silktouch" ) },
+		id: 26,
+		color: "white"
+	},
+	{
+		name: "silktouch",
+		weight: 1,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
+		minEnchant: function(){  return 15;},
+		canEnchant: function(mId,iId) { return iId >= 0 && iId <= 5; },
+		applies: function( other ) { return !( other === "fortune" || other === "silktouch" || other === "looting" || other === "luckofthesea" ) },
+		id: 27,
+		color: "darkviolet"
+	},
+	{
+		name: "luckofthesea",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "luckofthesea" },
+		id: 28,
+		color: "pink"
+	},
+	{
+		name: "lure",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "lure" },
+		id: 29,
+		color: "pink"
+	},
+	{
+		name: "unbreaking",
+		weight: 5,
+		minlevel: 1,
+		maxlevel: 3,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
+		minEnchant: function( i ){  return 5+(i-1)*8;},
+		canEnchant: function(mId,iId) { return iId >= 0 && iId <=14},
+		applies: function( other ) { return other !== "unbreaking" },
+		id: 30,
+		color: "turquoise"
+	},
+	{
+		name: "mending",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return this.minEnchant(i) + 50;},
+		minEnchant: function( i ){  return 5+(i-1)*8;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "mending" || other !== "infinity" },
+		id: 31,
+		color: "turquoise"
+	},
+	{
+		name: "curseofvanishing",
+		weight: 1,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "curseofvanishing" },
+		id: 32,
+		color: "pink"
+	},
+	{
+		name: "channeling",
+		weight: 1,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "channeling" },
+		id: 33,
+		color: "pink"
+	},
+	{
+		name: "impaling",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "impaling" },
+		id: 34,
+		color: "pink"
+	},
+	{
+		name: "loyalty",
+		weight: 5,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "loyalty" },
+		id: 35,
+		color: "pink"
+	},
+	{
+		name: "riptide",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "riptide" },
+		id: 36,
+		color: "pink"
+	},
+	{
+		name: "multishot",
+		weight: 2,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "multishot" },
+		id: 37,
+		color: "pink"
+	},
+	{
+		name: "piercing",
+		weight: 10,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "piercing" },
+		id: 38,
+		color: "pink"
+	},
+	{
+		name: "quickcharge",
+		weight: 5,
+		minlevel: 1,
+		maxlevel: 1,
+		maxEnchant: function( i ){ return 1;},
+		minEnchant: function( i ){  return 1;},
+		canEnchant: function(mId,iId) { return iId === 1; },
+		applies: function( other ) { return other !== "quickcharge" },
+		id: 39,
+		color: "pink"
+	},
+
 ];
